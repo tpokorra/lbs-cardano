652b6bfe79c495c18386d1121555ffabc02b7ddf
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-08-13 07:32:00 +0200

    store current tip to logs directory

--- cardano-bin/checkPassiveNode.sh
+++ cardano-bin/checkPassiveNode.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-export CARDANO_NODE_SOCKET_PATH=/home/cardano/passive/node.socket
+export CARDANO_NODE_PATH=/home/cardano/passive
+export CARDANO_NODE_SOCKET_PATH=$CARDANO_NODE_PATH/node.socket
 export PATH=$PATH:/usr/share/cardano
 
 systemctl status cardano-passive | cat
 cardano-cli --version
-cardano-cli query tip --mainnet
+
+online=1
+cardano-cli query tip --mainnet || online=0
+if [[ $online -eq 1 ]]
+then
+  json=`cardano-cli query tip --mainnet`
+  echo $json > $CARDANO_NODE_PATH/logs/current_tip.log
+fi
 
 echo
 echo
+
 free -mh
