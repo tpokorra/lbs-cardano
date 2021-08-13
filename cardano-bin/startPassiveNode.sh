ccb9861274d34673edae108bd78efd799051f0f1
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-08-13 06:31:45 +0200

    add parameters for managing memory better fixes #1

--- cardano-bin/startPassiveNode.sh
+++ cardano-bin/startPassiveNode.sh
@@ -6,6 +6,7 @@ CONFPATH="/home/cardano/passive"
 PATH=$PATH:/usr/share/cardano
 
 cardano-node run \
+   +RTS -N2 --disable-delayed-os-memory-return -I0.3 -Iw600 -A16m -F1.5 -H2500M -T -S -RTS \
    --topology $CONFPATH/mainnet-topology.json \
    --database-path $CONFPATH/db \
    --socket-path $CONFPATH/node.socket \
