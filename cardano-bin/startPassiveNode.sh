21344f854aa4dc0e11c50abfcf4c808160f2a75b
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-08-25 15:09:05 +0200

    even with special parameters it does not work on 4GB RAM. works fine on 8GB RAM without tweaking. related to #1

--- cardano-bin/startPassiveNode.sh
+++ cardano-bin/startPassiveNode.sh
@@ -5,8 +5,8 @@ PUBLICPORT="3001"
 CONFPATH="/home/cardano/passive"
 PATH=$PATH:/usr/share/cardano
 
+# +RTS -N2 --disable-delayed-os-memory-return -I0.3 -Iw600 -A16m -F1.5 -H2500M -T -S -RTS \
 cardano-node run \
-   +RTS -N2 --disable-delayed-os-memory-return -I0.3 -Iw600 -A16m -F1.5 -H2500M -T -S -RTS \
    --topology $CONFPATH/mainnet-topology.json \
    --database-path $CONFPATH/db \
    --socket-path $CONFPATH/node.socket \
