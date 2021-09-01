ceae32886e361404b5ca29f717dda12e2a04399e
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-09-01 20:08:50 +0200

    for node in mainnet, get initial json topology files

--- cardano-bin/startPassiveNode.sh
+++ cardano-bin/startPassiveNode.sh
@@ -5,6 +5,17 @@ PUBLICPORT="3001"
 CONFPATH="/home/cardano/passive"
 PATH=$PATH:/usr/share/cardano
 
+if [ ! -d $CONFPATH ]; then
+    mkdir -p $CONFPATH
+    cd $CONFPATH
+    wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-config.json
+    wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-byron-genesis.json
+    wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-shelley-genesis.json
+    wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-alonzo-genesis.json
+    wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-topology.json
+    cd -
+fi
+
 # +RTS -N2 --disable-delayed-os-memory-return -I0.3 -Iw600 -A16m -F1.5 -H2500M -T -S -RTS \
 cardano-node run \
    --topology $CONFPATH/mainnet-topology.json \
